cf91adbd0f772e44f99a343851f0e1b677baf531
AweAwe95 <vitalikarach95@gmail.com>
2021-11-13 22:02:12 +0300

    add responsive header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,19 @@
-import {NavLink, Route} from "react-router-dom";
-import {PageNotFound} from "../Main/PageNotFound/PageNotFound";
-import {Authorization} from "../Main/Authorization/Authorization";
-import {Registration} from "../Main/Registration/Registration";
-import {Profile} from "../Main/Profile/Profile";
-import {RecoveryPassword} from "../Main/RecoveryPassword/RecoveryPassword";
-import {NewPassword} from "../Main/NewPassword/NewPassword";
+import {NavLink} from "react-router-dom";
 import React from "react";
+import './Header.css'
 
 export function Header() {
     return (
-        <div>
-            <NavLink to="/">Profile</NavLink>{"  "}
-            <NavLink to="/auth">Authorization</NavLink>{"  "}
-            <NavLink to="/signup">Registration</NavLink>{"  "}
-            <NavLink to="/password-recovery">Password Recovery</NavLink>{"  "}
-            <NavLink to="/new-password">New Password</NavLink>{"  "}
+        <div className="header">
+            <NavLink to={'#'} className="logo">Site Logo</NavLink>
+            <div className="header-right">
+                <NavLink to="/">Profile</NavLink>
+                <NavLink to="/auth">Authorization</NavLink>
+                <NavLink to="/signup">Registration</NavLink>
+                <NavLink to="/password-recovery">Password Recovery</NavLink>
+                <NavLink to="/new-password">New Password</NavLink>
+                <NavLink to="/error-404">Error 404</NavLink>
+            </div>
         </div>
     )
 }
