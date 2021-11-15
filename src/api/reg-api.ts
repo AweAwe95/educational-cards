e8f5b729ec8b141c2b7afcf6c8b51266db4381ec
Ilya <IlyaDuginov1997@gmail.com>
2021-11-15 22:49:16 +0300

    add auth page

--- src/api/reg-api.ts
+++ src/api/reg-api.ts
@@ -1,4 +1,20 @@
 import axios from "axios";
+import { AuthFormikType } from "../components/Main/Authorization/AuthorizationForm";
+
+export type LoginDataType = {
+    created: string
+    email: string
+    isAdmin: boolean
+    name: string
+    publicCardPacksCount: number
+    rememberMe: boolean
+    token: string
+    tokenDeathTime: number
+    updated: string
+    verified: boolean
+    __v: number
+    _id: string
+}
 
 const instance = axios.create({
     baseURL: "https://neko-back.herokuapp.com/2.0",
@@ -8,5 +24,11 @@ const instance = axios.create({
 export const regApi = {
     regUser(email: string, password: string) {
         return instance.post('/auth/register', {email,password})
+    },
+    login(data: AuthFormikType) {
+        return instance.post<LoginDataType>('/auth/login', {...data})
+            .then(res => {
+                return res.data
+            })
     }
 }
