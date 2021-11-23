0b83b4b4b47bd4461777192679db88a3c3a0c8ad
AweAwe95 <vitalikarach95@gmail.com>
2021-11-23 19:53:49 +0300

    refactoring api

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,31 +2,31 @@ import axios from "axios";
 import {AuthFormikType} from "../components/Main/Authorization/AuthorizationForm/AuthorizationForm";
 
 const instance = axios.create({
-    baseURL: "https://neko-back.herokuapp.com/2.0",
+    baseURL: "https://neko-back.herokuapp.com/2.0/",
     withCredentials: true,
 })
 
 export const api = {
     regUser(email: string, password: string) {
-        return instance.post('/auth/register', {email, password})
+        return instance.post('auth/register', {email, password})
     },
     emailUser(email: string, from: string, message: string) {
-        return instance.post('/auth/forgot', {email, from, message})
+        return instance.post('auth/forgot', {email, from, message})
     },
     resetUser(password: string, resetPasswordToken: string | undefined) {
-        return instance.post('/auth/set-new-password', {password, resetPasswordToken})
+        return instance.post('auth/set-new-password', {password, resetPasswordToken})
     },
     login(data: AuthFormikType) {
-        return instance.post<LoginDataType>('/auth/login', {...data})
+        return instance.post<LoginDataType>('auth/login', {...data})
             .then(res => {
                 return res.data
             })
     },
     isAuthorized() {
-        return instance.post('/auth/me', {})
+        return instance.post('auth/me', {})
     },
     logout() {
-        return instance.delete('/auth/me', {})
+        return instance.delete('auth/me', {})
     }
 }
 
